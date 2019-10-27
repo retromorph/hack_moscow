f4e79773ad7dbcd9c2e0ad9988ebd7b5967cfd10
Increative <awesome.commerce@yandex.ru>
2019-10-27 09:22:42 +0300

    fixed cors

--- app/ui/DashboardMainActivity/DashboardMainActivity.ts
+++ app/ui/DashboardMainActivity/DashboardMainActivity.ts
@@ -5,38 +5,37 @@
 (document.querySelector(".buttonCard") as HTMLInputElement).addEventListener("click", function () {
     (document.querySelector(".CardsGridTwo") as HTMLInputElement).style.display = "none";
 
-    var request = {
-        "points": {
-            "points": [
-                {
-                    "x": 1,
-                    "y": 2
-                },
-                {
-                    "x": 2,
-                    "y": 9
-                },
-                {
-                    "x": 3,
-                    "y": 1
-                }
-            ]
-        }
-    }
+    var request =  "points={\n" +
+        "        \"points\": [\n" +
+        "            {\n" +
+        "                \"x\": 1,\n" +
+        "                \"y\": 2\n" +
+        "            },\n" +
+        "            {\n" +
+        "                \"x\": 2,\n" +
+        "                \"y\": 9\n" +
+        "            },\n" +
+        "            {\n" +
+        "                \"x\": 3,\n" +
+        "                \"y\": 1\n" +
+        "            }\n" +
+        "        ]\n" +
+        "    }";
+
 
     var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "http://lovushka.herokuapp.com/api/v1/graph", true);
+    xhr.withCredentials = false;
+    xhr.open("GET", "http://127.0.0.1/api/v1/graph", true);
 
     xhr.onreadystatechange = function (e) {
         if (xhr.readyState != 4) return;
 
         if (xhr.status != 200) {
-            alert(xhr.status + ': ' + xhr.statusText);
+            // alert(xhr.status + ': ' + xhr.statusText);
         } else {
             (document.querySelector("#graph") as HTMLInputElement).src = xhr.responseText
         }
     }
 
-    xhr.send(JSON.stringify(request))
+    xhr.send(request)
 });
